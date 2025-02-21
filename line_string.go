8ca5f2281f00fef451f1ea42c26573da354d33d9
pchchv <ipchchv@gmail.com>
2025-02-21 10:54:03 +0300

    Add Bound and Equal methods to LineString type

--- line_string.go
+++ line_string.go
@@ -2,3 +2,15 @@ package geo
 
 // LineString represents a set of points to be thought of as a polyline.
 type LineString []Point
+
+// Bound returns a rect around the line string.
+// Uses rectangular coordinates.
+func (ls LineString) Bound() Bound {
+	return MultiPoint(ls).Bound()
+}
+
+// Equal compares two line strings.
+// Returns true if lengths are the same and all points are Equal.
+func (ls LineString) Equal(lineString LineString) bool {
+	return MultiPoint(ls).Equal(MultiPoint(lineString))
+}
