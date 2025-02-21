80bb55aa91fb5c6ae6e076cfa417ece616b739a9
pchchv <ipchchv@gmail.com>
2025-02-21 11:08:10 +0300

    Add Orientation constants and type definition for polygon point order

--- define.go
+++ define.go
@@ -1,6 +1,16 @@
 package geo
 
+// Constants to define orientation.
+// They follow the right hand rule for orientation.
+const (
+	CW  Orientation = -1 // stands for Clock Wise
+	CCW Orientation = 1  // stands for Counter Clock Wise
+)
+
 // Pointer is something that can be represented by a point.
 type Pointer interface {
 	Point() Point
 }
+
+// Orientation defines the order of the points in a polygon or closed ring.
+type Orientation int8
