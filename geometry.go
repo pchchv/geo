b004d3e8d0531964fdeed5ab679c27878c2700b4
pchchv <ipchchv@gmail.com>
2025-02-21 10:35:02 +0300

    Add compile-time checks for Geometry interface implementations

--- geometry.go
+++ geometry.go
@@ -1,5 +1,12 @@
 package geo
 
+// compile time checks
+var (
+	_ Geometry = Point{}
+	_ Geometry = Bound{}
+	_ Geometry = Collection{}
+)
+
 // Geometry represents the shared attributes of a geometry.
 type Geometry interface {
 	GeoJSONType() string
@@ -7,3 +14,7 @@ type Geometry interface {
 	Bound() Bound
 	private() // requiring because sub package type switch over all possible types
 }
+
+func (p Point) private()      {}
+func (b Bound) private()      {}
+func (c Collection) private() {}
