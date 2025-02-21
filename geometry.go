06bd060fd06a6d1d0c5d6bb7e9f280e7c4cf3ab6
pchchv <ipchchv@gmail.com>
2025-02-21 10:37:21 +0300

    Add Bound and Dimensions methods to Collection type

--- geometry.go
+++ geometry.go
@@ -21,3 +21,46 @@ func (c Collection) private() {}
 
 // Collection is a collection of geometries that is also a Geometry.
 type Collection []Geometry
+
+// Bound returns the bounding box of all the Geometries combined.
+func (c Collection) Bound() Bound {
+	if len(c) == 0 {
+		return emptyBound
+	}
+
+	var b Bound
+	start := -1
+	for i, g := range c {
+		if g != nil {
+			start = i
+			b = g.Bound()
+			break
+		}
+	}
+
+	if start == -1 {
+		return emptyBound
+	}
+
+	for i := start + 1; i < len(c); i++ {
+		if c[i] == nil {
+			continue
+		}
+
+		b = b.Union(c[i].Bound())
+	}
+
+	return b
+}
+
+// Dimensions returns the max of the dimensions of the collection.
+func (c Collection) Dimensions() (max int) {
+	max--
+	for _, g := range c {
+		if d := g.Dimensions(); d > max {
+			max = d
+		}
+	}
+
+	return
+}
