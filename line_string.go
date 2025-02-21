48dd273b680f0dbdd55cc17a482c075f83da48f9
pchchv <ipchchv@gmail.com>
2025-02-21 10:55:08 +0300

    Add GeoJSONType and Dimensions methods to LineString type

--- line_string.go
+++ line_string.go
@@ -14,3 +14,13 @@ func (ls LineString) Bound() Bound {
 func (ls LineString) Equal(lineString LineString) bool {
 	return MultiPoint(ls).Equal(MultiPoint(lineString))
 }
+
+// GeoJSONType returns the GeoJSON type for the object.
+func (ls LineString) GeoJSONType() string {
+	return "LineString"
+}
+
+// Dimensions returns 1 because a LineString is a 1d object.
+func (ls LineString) Dimensions() int {
+	return 1
+}
