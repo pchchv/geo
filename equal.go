8578e5ed0dbe8176fec9b4d088e9caa52eb879a5
pchchv <ipchchv@gmail.com>
2025-02-22 10:14:19 +0300

    Add Equal function for comparing geometry types

---
+++ equal.go
@@ -0,0 +1,49 @@
+package geo
+
+import "fmt"
+
+// Equal returns if the two geometrires are equal.
+func Equal(g1, g2 Geometry) bool {
+	if g1 == nil || g2 == nil {
+		return g1 == g2
+	}
+
+	if g1.GeoJSONType() != g2.GeoJSONType() {
+		return false
+	}
+
+	switch g1 := g1.(type) {
+	case Point:
+		return g1.Equal(g2.(Point))
+	case MultiPoint:
+		return g1.Equal(g2.(MultiPoint))
+	case LineString:
+		return g1.Equal(g2.(LineString))
+	case MultiLineString:
+		return g1.Equal(g2.(MultiLineString))
+	case Ring:
+		if g2, ok := g2.(Ring); !ok {
+			return false
+		} else {
+			return g1.Equal(g2)
+		}
+	case Polygon:
+		if g2, ok := g2.(Polygon); !ok {
+			return false
+		} else {
+			return g1.Equal(g2)
+		}
+	case MultiPolygon:
+		return g1.Equal(g2.(MultiPolygon))
+	case Collection:
+		return g1.Equal(g2.(Collection))
+	case Bound:
+		if g2, ok := g2.(Bound); !ok {
+			return false
+		} else {
+			return g1.Equal(g2)
+		}
+	}
+
+	panic(fmt.Sprintf("geometry type not supported: %T", g1))
+}
