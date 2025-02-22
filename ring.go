fbfd3ea9842dbf772c831056873d8a858dbfa642
pchchv <ipchchv@gmail.com>
2025-02-22 09:47:21 +0300

    Add Bound and Equal methods to Ring type for bounding box and equality comparison

--- ring.go
+++ ring.go
@@ -2,3 +2,15 @@ package geo
 
 // Ring represents a set of ring on the earth.
 type Ring LineString
+
+// Bound returns a rect around the ring.
+// Uses rectangular coordinates.
+func (r Ring) Bound() Bound {
+	return MultiPoint(r).Bound()
+}
+
+// Equal compares two rings.
+// Returns true if lengths are the same and all points are Equal.
+func (r Ring) Equal(ring Ring) bool {
+	return MultiPoint(r).Equal(MultiPoint(ring))
+}
