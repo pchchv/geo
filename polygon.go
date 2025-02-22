14c189a1927f3c03493aea7632bf96297838ee96
pchchv <ipchchv@gmail.com>
2025-02-22 09:57:34 +0300

    Add Bound and Equal methods to Polygon type

--- polygon.go
+++ polygon.go
@@ -6,3 +6,27 @@ package geo
 // Each LineString is expected to be closed
 // ie. the first point matches the last.
 type Polygon []Ring
+
+// Bound returns a bound around the polygon.
+func (p Polygon) Bound() Bound {
+	if len(p) == 0 {
+		return emptyBound
+	}
+	return p[0].Bound()
+}
+
+// Equal compares two polygons.
+// Returns true if lengths are the same and all points are Equal.
+func (p Polygon) Equal(polygon Polygon) bool {
+	if len(p) != len(polygon) {
+		return false
+	}
+
+	for i := range p {
+		if !p[i].Equal(polygon[i]) {
+			return false
+		}
+	}
+
+	return true
+}
