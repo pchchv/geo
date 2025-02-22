28e943f080ef63b13dfc1507a8f226c3f89c8e38
pchchv <ipchchv@gmail.com>
2025-02-22 10:02:51 +0300

    Add Bound and Equal methods to MultiPolygon type

--- multi_polygon.go
+++ multi_polygon.go
@@ -2,3 +2,32 @@ package geo
 
 // MultiPolygon is a set of polygons.
 type MultiPolygon []Polygon
+
+// Bound returns a bound around the multi-polygon.
+func (mp MultiPolygon) Bound() Bound {
+	if len(mp) == 0 {
+		return emptyBound
+	}
+
+	bound := mp[0].Bound()
+	for i := 1; i < len(mp); i++ {
+		bound = bound.Union(mp[i].Bound())
+	}
+
+	return bound
+}
+
+// Equal compares two multi-polygons.
+func (mp MultiPolygon) Equal(multiPolygon MultiPolygon) bool {
+	if len(mp) != len(multiPolygon) {
+		return false
+	}
+
+	for i, p := range mp {
+		if !p.Equal(multiPolygon[i]) {
+			return false
+		}
+	}
+
+	return true
+}
