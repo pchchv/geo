57500f55ea745c5ff047637bd88acd1f0b6edff3
pchchv <ipchchv@gmail.com>
2025-02-24 07:59:36 +0300

    Add CustomJSONMarshaler for flexible JSON marshaling

--- geojson/json.go
+++ geojson/json.go
@@ -2,29 +2,62 @@ package geojson
 
 import "encoding/json"
 
-// CustomJSONUnmarshaler can be set to have the code use a
-// different json unmarshaler than the default in the standard library.
-// One use case in enabling `github.com/json-iterator/go`
-// with something like this:
-//
-//	import (
-//	  jsoniter "github.com/json-iterator/go"
-//	  "github.com/pchchv/geo"
-//	)
-//
-//	var c = jsoniter.Config{
-//	  EscapeHTML:              true,
-//	  SortMapKeys:             false,
-//	  MarshalFloatWith6Digits: true,
-//	}.Froze()
-//
-//	geo.CustomJSONMarshaler = c
-//	geo.CustomJSONUnmarshaler = c
-//
-// Note that any errors encountered during unmarshaling will be different.
-var CustomJSONUnmarshaler interface {
-	Unmarshal(data []byte, v interface{}) error
-} = nil
+var (
+	// CustomJSONMarshaler can be set to have the code use a
+	// different json marshaler than the default in the standard library.
+	// One use case in enabling `github.com/json-iterator/go`
+	// with something like this:
+	//
+	//	import (
+	//	  jsoniter "github.com/json-iterator/go"
+	//	  "github.com/pchchv/geo"
+	//	)
+	//
+	//	var c = jsoniter.Config{
+	//	  EscapeHTML:              true,
+	//	  SortMapKeys:             false,
+	//	  MarshalFloatWith6Digits: true,
+	//	}.Froze()
+	//
+	//	geo.CustomJSONMarshaler = c
+	//	geo.CustomJSONUnmarshaler = c
+	//
+	// Any errors encountered during marshaling will be different.
+	CustomJSONMarshaler interface {
+		Marshal(v interface{}) ([]byte, error)
+	} = nil
+	// CustomJSONUnmarshaler can be set to have the code use a
+	// different json unmarshaler than the default in the standard library.
+	// One use case in enabling `github.com/json-iterator/go`
+	// with something like this:
+	//
+	//	import (
+	//	  jsoniter "github.com/json-iterator/go"
+	//	  "github.com/pchchv/geo"
+	//	)
+	//
+	//	var c = jsoniter.Config{
+	//	  EscapeHTML:              true,
+	//	  SortMapKeys:             false,
+	//	  MarshalFloatWith6Digits: true,
+	//	}.Froze()
+	//
+	//	geo.CustomJSONMarshaler = c
+	//	geo.CustomJSONUnmarshaler = c
+	//
+	// Any errors encountered during unmarshaling will be different.
+	CustomJSONUnmarshaler interface {
+		Unmarshal(data []byte, v interface{}) error
+	} = nil
+)
+
+func marshalJSON(v interface{}) ([]byte, error) {
+	if CustomJSONMarshaler == nil {
+		return json.Marshal(v)
+	}
+
+	return CustomJSONMarshaler.Marshal(v)
+}
 
 func unmarshalJSON(data []byte, v interface{}) error {
 	if CustomJSONUnmarshaler == nil {
