de795d949bf673725399ee2d750e176d8fd83cb6
pchchv <ipchchv@gmail.com>
2025-02-24 09:28:04 +0300

    Add propertiesTestFeature function to enhance Feature type testing

--- geojson/properties_test.go
+++ geojson/properties_test.go
@@ -17,3 +17,23 @@ func TestPropertiesClone(t *testing.T) {
 		t.Errorf("should clone properties")
 	}
 }
+
+func propertiesTestFeature() *Feature {
+	rawJSON := `
+	  { "type": "Feature",
+	    "geometry": {
+	      "type": "Point",
+	      "coordinates": [102.0, 0.5]
+	    },
+	    "properties": {
+	      "bool":true,
+	      "falsebool":false,
+	      "int": 1,
+	      "float64": 1.2,
+	      "string":"text"
+	    }
+	  }`
+
+	f, _ := UnmarshalFeature([]byte(rawJSON))
+	return f
+}
