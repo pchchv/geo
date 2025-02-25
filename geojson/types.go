3498843f7352d83e5ce17901fa3a3cb025f7fe28
pchchv <ipchchv@gmail.com>
2025-02-25 10:48:33 +0300

    Add supported GeoJSON types constants

---
+++ geojson/types.go
@@ -0,0 +1,11 @@
+package geojson
+
+// List of the geojson types that are currently supported.
+const (
+	TypePoint           = "Point"
+	TypeMultiPoint      = "MultiPoint"
+	TypeLineString      = "LineString"
+	TypeMultiLineString = "MultiLineString"
+	TypePolygon         = "Polygon"
+	TypeMultiPolygon    = "MultiPolygon"
+)
