ba5b169d663f12b8e5176de90832c01772894113
pchchv <ipchchv@gmail.com>
2025-02-25 11:16:43 +0300

    Add function to calculate centroid of MultiPoint geometry

--- planar/area.go
+++ planar/area.go
@@ -1,1 +1,18 @@
 package planar
+
+import "github.com/pchchv/geo"
+
+func multiPointCentroid(mp geo.MultiPoint) geo.Point {
+	if len(mp) == 0 {
+		return geo.Point{}
+	}
+
+	x, y := 0.0, 0.0
+	for _, p := range mp {
+		x += p[0]
+		y += p[1]
+	}
+
+	num := float64(len(mp))
+	return geo.Point{x / num, y / num}
+}
