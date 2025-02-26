64f9c8c2119b85a359edb3e0563ec2e97b30595a
pchchv <ipchchv@gmail.com>
2025-02-26 10:39:34 +0300

    Add tests for DistanceFrom function with LineString, Polygon, and MultiPoint cases

--- planar/distance_from_test.go
+++ planar/distance_from_test.go
@@ -1,10 +1,13 @@
 package planar
 
 import (
+	"math"
 	"testing"
 
 	"github.com/pchchv/geo"
 )
+
+const epsilon = 1e-6
 
 func TestDistanceFromSegment(t *testing.T) {
 	a := geo.Point{0, 0}
@@ -59,5 +62,83 @@ func TestDistanceFromSegment(t *testing.T) {
 func TestDistanceFromWithIndex(t *testing.T) {
 	for _, g := range geo.AllGeometries {
 		DistanceFromWithIndex(g, geo.Point{})
+	}
+}
+
+func TestDistanceFrom_LineString(t *testing.T) {
+	ls := geo.LineString{{0, 0}, {0, 3}, {4, 3}, {4, 0}}
+	cases := []struct {
+		name   string
+		point  geo.Point
+		result float64
+	}{
+		{
+			point:  geo.Point{4.5, 1.5},
+			result: 0.5,
+		},
+		{
+			point:  geo.Point{0.4, 1.5},
+			result: 0.4,
+		},
+		{
+			point:  geo.Point{-0.3, 1.5},
+			result: 0.3,
+		},
+		{
+			point:  geo.Point{0.3, 2.8},
+			result: 0.2,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			d := DistanceFrom(ls, tc.point)
+			if math.Abs(d-tc.result) > epsilon {
+				t.Errorf("incorrect distance: %v != %v", d, tc.result)
+			}
+		})
+	}
+}
+
+func TestDistanceFrom_Polygon(t *testing.T) {
+	r1 := geo.Ring{{0, 0}, {3, 0}, {3, 3}, {0, 3}, {0, 0}}
+	r2 := geo.Ring{{1, 1}, {2, 1}, {2, 2}, {1, 2}, {1, 1}}
+	poly := geo.Polygon{r1, r2}
+	cases := []struct {
+		name   string
+		point  geo.Point
+		result float64
+	}{
+		{
+			name:   "outside",
+			point:  geo.Point{-1, 2},
+			result: 1,
+		},
+		{
+			name:   "inside",
+			point:  geo.Point{0.4, 2},
+			result: 0.4,
+		},
+		{
+			name:   "in hole",
+			point:  geo.Point{1.3, 1.4},
+			result: 0.3,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			if d := DistanceFrom(poly, tc.point); math.Abs(d-tc.result) > epsilon {
+				t.Errorf("incorrect distance: %v != %v", d, tc.result)
+			}
+		})
+	}
+}
+
+func TestDistanceFrom_MultiPoint(t *testing.T) {
+	mp := geo.MultiPoint{{0.0}, {1, 1}, {2, 2}}
+	fromPoint := geo.Point{3, 2}
+	if distance := DistanceFrom(mp, fromPoint); distance != 1 {
+		t.Errorf("distance incorrect: %v != %v", distance, 1)
 	}
 }
