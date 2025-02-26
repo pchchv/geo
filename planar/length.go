3d77dc48a0ea733643ef166c332fcab708dec62c
pchchv <ipchchv@gmail.com>
2025-02-26 11:16:53 +0300

    Implement Length function for 2D Euclidean geometry boundary calculation

--- planar/length.go
+++ planar/length.go
@@ -1,1 +1,12 @@
 package planar
+
+import (
+	"github.com/pchchv/geo"
+	"github.com/pchchv/geo/internal/length"
+)
+
+// Length returns the length of the boundary of
+// the geometry using 2d euclidean geometry.
+func Length(g geo.Geometry) float64 {
+	return length.Length(g, Distance)
+}
