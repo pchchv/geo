a3678fcb59cca3af9d2331158898fcf5da51cf71
pchchv <ipchchv@gmail.com>
2025-02-26 11:09:16 +0300

    Add examples for Area and Distance functions in planar package

---
+++ planar/example_test.go
@@ -0,0 +1,31 @@
+package planar_test
+
+import (
+	"fmt"
+
+	"github.com/pchchv/geo"
+	"github.com/pchchv/geo/planar"
+)
+
+func ExampleArea() {
+	// +
+	// |\
+	// | \
+	// |  \
+	// +---+
+
+	r := geo.Ring{{0, 0}, {3, 0}, {0, 4}, {0, 0}}
+	a := planar.Area(r)
+
+	fmt.Println(a)
+	// Output:
+	// 6
+}
+
+func ExampleDistance() {
+	d := planar.Distance(geo.Point{0, 0}, geo.Point{3, 4})
+
+	fmt.Println(d)
+	// Output:
+	// 5
+}
