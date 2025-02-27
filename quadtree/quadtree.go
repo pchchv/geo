e64cd6bf3d57ec715510c9d61061c93be520536d
pchchv <ipchchv@gmail.com>
2025-02-27 09:36:35 +0300

    Add node struct to quadtree package for quad tree representation

--- quadtree/quadtree.go
+++ quadtree/quadtree.go
@@ -4,3 +4,10 @@ import "github.com/pchchv/geo"
 
 // FilterFunc is a function that filters the points to search for.
 type FilterFunc func(p geo.Pointer) bool
+
+// node represents a node of the quad tree.
+// Each node stores a Value and has links to its 4 children.
+type node struct {
+	Value    geo.Pointer
+	Children [4]*node
+}
