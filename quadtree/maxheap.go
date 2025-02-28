ee3fd8b8c54f7b2f7bb3912625acfa3d0ef6c2c8
pchchv <ipchchv@gmail.com>
2025-02-28 09:24:48 +0300

    Add maxHeap type for managing nearest points in Quadtree

--- quadtree/maxheap.go
+++ quadtree/maxheap.go
@@ -6,3 +6,10 @@ type heapItem struct {
 	point    geo.Pointer
 	distance float64
 }
+
+// maxHeap is used for the knearest list.
+// Needed a way to keep the furthest point from the query point in the list,
+// so maxHeap is used.
+// When a point is found closer than the furthest point,
+// remove furthest and add the new point to the heap.
+type maxHeap []heapItem
