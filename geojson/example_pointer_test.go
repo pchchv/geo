5838f626b78e2f24540345192400fff00d538daa
pchchv <ipchchv@gmail.com>
2025-02-28 08:58:07 +0300

    Add Point method to CentroidPoint for representative point calculation

--- geojson/example_pointer_test.go
+++ geojson/example_pointer_test.go
@@ -5,3 +5,10 @@ import "github.com/pchchv/geo/geojson"
 type CentroidPoint struct {
 	*geojson.Feature
 }
+
+func (cp CentroidPoint) Point() geo.Point {
+	// this is where you would decide how to define
+	// the representative point of the feature.
+	c, _ := planar.CentroidArea(cp.Geometry)
+	return c
+}
