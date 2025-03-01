495817291bbf69a095a5b14a4272e0307730e8c3
pchchv <ipchchv@gmail.com>
2025-03-01 12:04:27 +0300

    Add WGS84 projection implementation for converting WGS to Mercator

--- project/projections.go
+++ project/projections.go
@@ -8,14 +8,29 @@ import (
 
 const earthRadiusPi = geo.EarthRadius * math.Pi
 
-// Mercator performs the Spherical Pseudo-Mercator projection used by most web maps.
-var Mercator = struct {
-	ToWGS84 geo.Projection
-}{
-	ToWGS84: func(p geo.Point) geo.Point {
-		return geo.Point{
-			180.0 * p[0] / earthRadiusPi,
-			180.0 / math.Pi * (2*math.Atan(math.Exp(p[1]/geo.EarthRadius)) - math.Pi/2.0),
-		}
-	},
-}
+var (
+	// Mercator performs the Spherical Pseudo-Mercator projection used by most web maps.
+	Mercator = struct {
+		ToWGS84 geo.Projection
+	}{
+		ToWGS84: func(p geo.Point) geo.Point {
+			return geo.Point{
+				180.0 * p[0] / earthRadiusPi,
+				180.0 / math.Pi * (2*math.Atan(math.Exp(p[1]/geo.EarthRadius)) - math.Pi/2.0),
+			}
+		},
+	}
+	// WGS84 is what common uses lon/lat projection.
+	WGS84 = struct {
+		// ToMercator projections from WGS to Mercator, used by most web maps
+		ToMercator geo.Projection
+	}{
+		ToMercator: func(g geo.Point) geo.Point {
+			y := math.Log(math.Tan((90.0+g[1])*math.Pi/360.0)) * geo.EarthRadius
+			return geo.Point{
+				earthRadiusPi / 180.0 * g[0],
+				math.Max(-earthRadiusPi, math.Min(y, earthRadiusPi)),
+			}
+		},
+	}
+)
