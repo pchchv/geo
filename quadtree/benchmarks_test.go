8d47e41117be0df6f127b1ff9f1e85439b5ed064
pchchv <ipchchv@gmail.com>
2025-03-01 11:54:43 +0300

    Add benchmarks for Quadtree InBound method to evaluate performance

--- quadtree/benchmarks_test.go
+++ quadtree/benchmarks_test.go
@@ -64,3 +64,71 @@ func BenchmarkRandomFind1000Naive(b *testing.B) {
 		_ = best
 	}
 }
+
+func BenchmarkRandomInBound1000(b *testing.B) {
+	r := rand.New(rand.NewSource(43))
+	qt := New(geo.Bound{Min: geo.Point{0, 0}, Max: geo.Point{1, 1}})
+	for i := 0; i < 1000; i++ {
+		p := geo.Point{r.Float64(), r.Float64()}
+		if err := qt.Add(p); err != nil {
+			b.Fatalf("unexpected error for %v: %v", p, err)
+		}
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p := geo.Point{r.Float64(), r.Float64()}
+		qt.InBound(nil, p.Bound().Pad(0.1))
+	}
+}
+
+func BenchmarkRandomInBound1000Naive(b *testing.B) {
+	points := []geo.Point{}
+	r := rand.New(rand.NewSource(43))
+	qt := New(geo.Bound{Min: geo.Point{0, 0}, Max: geo.Point{1, 1}})
+	for i := 0; i < 1000; i++ {
+		p := geo.Point{r.Float64(), r.Float64()}
+		if err := qt.Add(p); err != nil {
+			b.Fatalf("unexpected error for %v: %v", p, err)
+		}
+
+		points = append(points, p)
+	}
+
+	var near []geo.Point
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p := geo.Point{r.Float64(), r.Float64()}
+		b := geo.Bound{Min: p, Max: p}
+		b = b.Pad(0.1)
+		near = near[:0]
+		for _, p := range points {
+			if b.Contains(p) {
+				near = append(near, p)
+			}
+		}
+
+		_ = len(near)
+	}
+}
+
+func BenchmarkRandomInBound1000Buf(b *testing.B) {
+	r := rand.New(rand.NewSource(43))
+	qt := New(geo.Bound{Min: geo.Point{0, 0}, Max: geo.Point{1, 1}})
+	for i := 0; i < 1000; i++ {
+		p := geo.Point{r.Float64(), r.Float64()}
+		if err := qt.Add(p); err != nil {
+			b.Fatalf("unexpected error for %v: %v", p, err)
+		}
+	}
+
+	var buf []geo.Pointer
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p := geo.Point{r.Float64(), r.Float64()}
+		buf = qt.InBound(buf, p.Bound().Pad(0.1))
+	}
+}
