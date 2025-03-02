0513252332022c608877a230dd00241b5e443233
pchchv <ipchchv@gmail.com>
2025-03-02 15:53:37 +0300

    Implement push function to manage MultiLineString points in clip package

--- clip/clip.go
+++ clip/clip.go
@@ -1,1 +1,12 @@
 package clip
+
+import "github.com/pchchv/geo"
+
+func push(out geo.MultiLineString, i int, p geo.Point) geo.MultiLineString {
+	if i >= len(out) {
+		out = append(out, geo.LineString{})
+	}
+
+	out[i] = append(out[i], p)
+	return out
+}
