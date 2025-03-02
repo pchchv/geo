79bfe07c078651105fbf8ccb35ee6d43e76c0b2d
pchchv <ipchchv@gmail.com>
2025-03-02 15:55:08 +0300

    Add bitCode and bitCodeOpen functions for point position evaluation relative to bounding box

--- clip/clip.go
+++ clip/clip.go
@@ -10,3 +10,41 @@ func push(out geo.MultiLineString, i int, p geo.Point) geo.MultiLineString {
 	out[i] = append(out[i], p)
 	return out
 }
+
+// bitCode returns the point position relative to the bbox:
+//
+//		     left  mid  right
+//		top  1001  1000  1010
+//		mid  0001  0000  0010
+//	 bottom  0101  0100  0110
+func bitCode(b geo.Bound, p geo.Point) (code int) {
+	if p[0] < b.Min[0] {
+		code |= 1
+	} else if p[0] > b.Max[0] {
+		code |= 2
+	}
+
+	if p[1] < b.Min[1] {
+		code |= 4
+	} else if p[1] > b.Max[1] {
+		code |= 8
+	}
+
+	return
+}
+
+func bitCodeOpen(b geo.Bound, p geo.Point) (code int) {
+	if p[0] <= b.Min[0] {
+		code |= 1
+	} else if p[0] >= b.Max[0] {
+		code |= 2
+	}
+
+	if p[1] <= b.Min[1] {
+		code |= 4
+	} else if p[1] >= b.Max[1] {
+		code |= 8
+	}
+
+	return
+}
