a5dc8f83bb75fee2a8cb97912b0661ad5f684208
pchchv <ipchchv@gmail.com>
2025-03-02 21:22:58 +0300

    Add LineString and MultiLineString functions for clipping to bounding box

--- clip/helpers.go
+++ clip/helpers.go
@@ -13,3 +13,45 @@ func Ring(b geo.Bound, r geo.Ring) geo.Ring {
 
 	return result
 }
+
+// LineString clips the linestring to the bounding box.
+func LineString(b geo.Bound, ls geo.LineString, opts ...Option) geo.MultiLineString {
+	var open bool
+	if len(opts) > 0 {
+		o := &options{}
+		for _, opt := range opts {
+			opt(o)
+		}
+
+		open = o.openBound
+	}
+
+	result := line(b, ls, open)
+	if len(result) == 0 {
+		return nil
+	}
+
+	return result
+}
+
+// MultiLineString clips the linestrings to the bounding box and returns a linestring union.
+func MultiLineString(b geo.Bound, mls geo.MultiLineString, opts ...Option) (result geo.MultiLineString) {
+	var open bool
+	if len(opts) > 0 {
+		o := &options{}
+		for _, opt := range opts {
+			opt(o)
+		}
+
+		open = o.openBound
+	}
+
+	for _, ls := range mls {
+		r := line(b, ls, open)
+		if len(r) != 0 {
+			result = append(result, r...)
+		}
+	}
+
+	return
+}
