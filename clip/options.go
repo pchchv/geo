d634964e37b237ab25a321030de3836005a331dc
pchchv <ipchchv@gmail.com>
2025-03-02 21:17:00 +0300

    Add OpenBound option to configure boundary treatment in clip operations

--- clip/options.go
+++ clip/options.go
@@ -3,6 +3,15 @@ package clip
 // Option is a possible parameter to the clip operations.
 type Option func(*options)
 
+// OpenBound is an option to treat the bound as open.
+// i.e. any lines along the bound sides will be
+// removed and a point on boundary will cause the line to be split.
+func OpenBound(yes bool) Option {
+	return func(o *options) {
+		o.openBound = yes
+	}
+}
+
 type options struct {
 	openBound bool
 }
