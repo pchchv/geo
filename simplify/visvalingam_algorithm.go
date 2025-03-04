96c907559c4a89b4fed684557ba5798051b6956f
pchchv <ipchchv@gmail.com>
2025-03-04 10:54:32 +0300

    Add VisvalingamThreshold and VisvalingamKeep methods for geometry simplification

--- simplify/visvalingam_algorithm.go
+++ simplify/visvalingam_algorithm.go
@@ -1,5 +1,7 @@
 package simplify
 
+import "math"
+
 // VisvalingamSimplifier is a reducer that performs the vivalingham algorithm.
 type VisvalingamSimplifier struct {
 	Threshold float64
@@ -17,4 +19,22 @@ func Visvalingam(threshold float64, minPointsToKeep int) *VisvalingamSimplifier 
 		Threshold: threshold,
 		ToKeep:    minPointsToKeep,
 	}
+}
+
+// VisvalingamThreshold runs the Visvalingam-Whyatt algorithm
+// removing triangles whose area is below the threshold.
+// Will keep at least 2 points for lines, 3 for non-closed rings and 4 for closed rings.
+// The intent is to maintain valid geometry after simplification,
+// however it is still possible for the simplification to create self-intersections.
+func VisvalingamThreshold(threshold float64) *VisvalingamSimplifier {
+	return Visvalingam(threshold, 0)
+}
+
+// VisvalingamKeep runs the Visvalingam-Wyatt algorithm,
+// removing triangles of minimum area until the number of points `minPointsToKeep` is reached.
+// If minPointsToKeep is 0, the algorithm will keep at least 2 points for lines,
+// 3 for unclosed rings, and 4 for closed rings.
+// However, it is still possible to create self-intersections when simplifying.
+func VisvalingamKeep(minPointsToKeep int) *VisvalingamSimplifier {
+	return Visvalingam(math.MaxFloat64, minPointsToKeep)
 }
