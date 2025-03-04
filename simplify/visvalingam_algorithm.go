bb83578ea1b22025e48406b043e320949a966a13
pchchv <ipchchv@gmail.com>
2025-03-04 11:01:21 +0300

    Implement Push, Pop, and Update methods for minHeap to enhance priority queue functionality in Visvalingam algorithm

--- simplify/visvalingam_algorithm.go
+++ simplify/visvalingam_algorithm.go
@@ -50,6 +50,37 @@ type visItem struct {
 // minHeap creates a priority queue or min heap.
 type minHeap []*visItem
 
+func (h *minHeap) Push(item *visItem) {
+	item.index = len(*h)
+	*h = append(*h, item)
+	h.up(item.index)
+}
+
+func (h *minHeap) Pop() *visItem {
+	removed := (*h)[0]
+	lastItem := (*h)[len(*h)-1]
+	(*h) = (*h)[:len(*h)-1]
+	if len(*h) > 0 {
+		lastItem.index = 0
+		(*h)[0] = lastItem
+		h.down(0)
+	}
+
+	return removed
+}
+
+func (h minHeap) Update(item *visItem, area float64) {
+	if item.area > area {
+		// area got smaller
+		item.area = area
+		h.up(item.index)
+	} else {
+		// area got larger
+		item.area = area
+		h.down(item.index)
+	}
+}
+
 func (h minHeap) up(i int) {
 	object := h[i]
 	for i > 0 {
