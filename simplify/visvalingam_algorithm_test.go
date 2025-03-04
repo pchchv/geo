21830748bac6665828f9a1038a1bc6bd662eba94
pchchv <ipchchv@gmail.com>
2025-03-04 11:17:42 +0300

    Add unit tests for VisvalingamThreshold and VisvalingamKeep functions to validate geometry simplification logic

--- simplify/visvalingam_algorithm_test.go
+++ simplify/visvalingam_algorithm_test.go
@@ -1,6 +1,7 @@
 package simplify
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/pchchv/geo"
@@ -44,6 +45,235 @@ func TestDoubleTriangleArea(t *testing.T) {
 			area := doubleTriangleArea(ls, tc.i1, tc.i2, tc.i3)
 			if area != expected {
 				t.Errorf("incorrect area: %v != %v", area, expected)
+			}
+		})
+	}
+}
+
+func TestVisvalingamThreshold(t *testing.T) {
+	cases := []struct {
+		name      string
+		threshold float64
+		ls        geo.LineString
+		expected  geo.LineString
+		indexMap  []int
+	}{
+		{
+			name:      "no reduction",
+			threshold: 0.9,
+			ls:        geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected:  geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			indexMap:  []int{0, 1, 2, 3, 4},
+		},
+		{
+			name:      "reduction",
+			threshold: 1.1,
+			ls:        geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected:  geo.LineString{{0, 0}, {0, 4}},
+			indexMap:  []int{0, 4},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			v, im := VisvalingamThreshold(tc.threshold).simplify(tc.ls, false, true)
+			if !v.Equal(tc.expected) {
+				t.Log(v)
+				t.Log(tc.expected)
+				t.Errorf("incorrect line")
+			}
+
+			if !reflect.DeepEqual(im, tc.indexMap) {
+				t.Log(im)
+				t.Log(tc.indexMap)
+				t.Errorf("incorrect index map")
+			}
+		})
+	}
+}
+
+func TestVisvalingamThreshold_area(t *testing.T) {
+	cases := []struct {
+		name     string
+		r        geo.Ring
+		expected geo.Ring
+		indexMap []int
+	}{
+		{
+			name:     "reduction",
+			r:        geo.Ring{{0, 0}, {1, 0}, {1, 0.5}, {1, 1}, {0.5, 1}, {0, 1}},
+			expected: geo.Ring{{0, 0}, {1, 0}, {0, 1}},
+			indexMap: []int{0, 1, 5},
+		},
+		{
+			name:     "reduction closed",
+			r:        geo.Ring{{0, 0}, {1, 0}, {1, 0.5}, {1, 1}, {0.5, 1}, {0, 1}, {0, 0}},
+			expected: geo.Ring{{0, 0}, {1, 0}, {1, 1}, {0, 0}},
+			indexMap: []int{0, 1, 3, 6},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			v, im := VisvalingamThreshold(10).simplify(geo.LineString(tc.r), true, true)
+			if !v.Equal(geo.LineString(tc.expected)) {
+				t.Log(v)
+				t.Log(tc.expected)
+				t.Errorf("incorrect ring")
+			}
+
+			if !reflect.DeepEqual(im, tc.indexMap) {
+				t.Log(im)
+				t.Log(tc.indexMap)
+				t.Errorf("incorrect index map")
+			}
+		})
+	}
+}
+
+func TestVisvalingamKeep(t *testing.T) {
+	cases := []struct {
+		name     string
+		keep     int
+		ls       geo.LineString
+		expected geo.LineString
+		indexMap []int
+	}{
+		{
+			name:     "keep 6",
+			keep:     6,
+			ls:       geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected: geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			indexMap: []int{0, 1, 2, 3, 4},
+		},
+		{
+			name:     "keep 5",
+			keep:     5,
+			ls:       geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected: geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			indexMap: []int{0, 1, 2, 3, 4},
+		},
+		{
+			name:     "keep 4",
+			keep:     4,
+			ls:       geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected: geo.LineString{{0, 0}, {0, 2}, {1, 3}, {0, 4}},
+			indexMap: []int{0, 2, 3, 4},
+		},
+		{
+			name:     "keep 3",
+			keep:     3,
+			ls:       geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected: geo.LineString{{0, 0}, {0, 2}, {0, 4}},
+			indexMap: []int{0, 2, 4},
+		},
+		{
+			name:     "keep 2",
+			keep:     2,
+			ls:       geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected: geo.LineString{{0, 0}, {0, 4}},
+			indexMap: []int{0, 4},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			v, im := VisvalingamKeep(tc.keep).simplify(tc.ls, false, true)
+			if !v.Equal(tc.expected) {
+				t.Log(v)
+				t.Log(tc.expected)
+				t.Errorf("incorrect line")
+			}
+
+			if !reflect.DeepEqual(im, tc.indexMap) {
+				t.Log(im)
+				t.Log(tc.indexMap)
+				t.Errorf("incorrect index map")
+			}
+		})
+	}
+}
+
+func TestVisvalingam(t *testing.T) {
+	cases := []struct {
+		name      string
+		threshold float64
+		keep      int
+		ls        geo.LineString
+		expected  geo.LineString
+		indexMap  []int
+	}{
+		{
+			name:      "keep nothing",
+			threshold: 1.1,
+			keep:      0,
+			ls:        geo.LineString{{0, 0}, {1, 1}, {0, 2}},
+			expected:  geo.LineString{{0, 0}, {0, 2}},
+			indexMap:  []int{0, 2},
+		},
+		{
+			name:      "keep everything",
+			threshold: 1.1,
+			keep:      3,
+			ls:        geo.LineString{{0, 0}, {1, 1}, {0, 2}},
+			expected:  geo.LineString{{0, 0}, {1, 1}, {0, 2}},
+			indexMap:  []int{0, 1, 2},
+		},
+		{
+			name:      "not meeting threshold",
+			threshold: 0.9,
+			keep:      0,
+			ls:        geo.LineString{{0, 0}, {1, 1}, {0, 2}},
+			expected:  geo.LineString{{0, 0}, {1, 1}, {0, 2}},
+			indexMap:  []int{0, 1, 2},
+		},
+		{
+			name:      "5 points keep nothing",
+			threshold: 1.1,
+			keep:      0,
+			ls:        geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected:  geo.LineString{{0, 0}, {0, 4}},
+			indexMap:  []int{0, 4},
+		},
+		{
+			name:      "5 points keep everything",
+			threshold: 1.1,
+			keep:      5,
+			ls:        geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected:  geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			indexMap:  []int{0, 1, 2, 3, 4},
+		},
+		{
+			name:      "5 points reduce to limit",
+			threshold: 1.1,
+			keep:      3,
+			ls:        geo.LineString{{0, 0}, {1, 1}, {0, 2}, {1, 3}, {0, 4}},
+			expected:  geo.LineString{{0, 0}, {0, 2}, {0, 4}},
+			indexMap:  []int{0, 2, 4},
+		},
+		{
+			name:      "removes colinear points",
+			threshold: 0.1,
+			keep:      0,
+			ls:        geo.LineString{{0, 0}, {0, 1}, {0, 2}},
+			expected:  geo.LineString{{0, 0}, {0, 2}},
+			indexMap:  []int{0, 2},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			v, im := Visvalingam(tc.threshold, tc.keep).simplify(tc.ls, false, true)
+			if !v.Equal(tc.expected) {
+				t.Log(v)
+				t.Log(tc.expected)
+				t.Errorf("incorrect line")
+			}
+
+			if !reflect.DeepEqual(im, tc.indexMap) {
+				t.Log(im)
+				t.Log(tc.indexMap)
+				t.Errorf("incorrect index map")
 			}
 		})
 	}
