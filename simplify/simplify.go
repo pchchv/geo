eb968a2b182571a66bd899b3ed9a3570b2d64b9d
pchchv <ipchchv@gmail.com>
2025-03-04 10:14:29 +0300

    Implement runSimplify function to streamline line simplification process

--- simplify/simplify.go
+++ simplify/simplify.go
@@ -5,3 +5,12 @@ import "github.com/pchchv/geo"
 type simplifier interface {
 	simplify(l geo.LineString, area bool, withIndexMap bool) (geo.LineString, []int)
 }
+
+func runSimplify(s simplifier, ls geo.LineString, area bool) geo.LineString {
+	if len(ls) <= 2 {
+		return ls
+	}
+
+	ls, _ = s.simplify(ls, area, false)
+	return ls
+}
