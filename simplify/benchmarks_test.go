ce2ad840141eff0f8dfd71a72e95aa7c7db50260
pchchv <ipchchv@gmail.com>
2025-03-05 10:16:17 +0300

    Add benchmarks for Radial simplification algorithm

--- simplify/benchmarks_test.go
+++ simplify/benchmarks_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/pchchv/geo"
+	"github.com/pchchv/geo/planar"
 )
 
 func TestDouglasPeucker_BenchmarkData(t *testing.T) {
@@ -96,6 +97,58 @@ func BenchmarkVisvalingam_Keep(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		VisvalingamKeep(toKeep).LineString(data[i])
+	}
+}
+
+func TestRadial_BenchmarkData(t *testing.T) {
+	cases := []struct {
+		threshold float64
+		length    int
+	}{
+		{0.1, 8282},
+		{0.5, 2023},
+		{1.0, 1043},
+		{1.5, 703},
+		{2.0, 527},
+		{3.0, 350},
+		{4.0, 262},
+		{5.0, 209},
+	}
+
+	ls := benchmarkData()
+	for i, tc := range cases {
+		r := Radial(planar.Distance, tc.threshold).LineString(ls.Clone())
+		if len(r) != tc.length {
+			t.Errorf("%d: data reduced poorly: %v != %v", i, len(r), tc.length)
+		}
+	}
+}
+
+func BenchmarkRadial(b *testing.B) {
+	var data []geo.LineString
+	ls := benchmarkData()
+	for i := 0; i < b.N; i++ {
+		data = append(data, ls.Clone())
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Radial(planar.Distance, 0.1).LineString(data[i])
+	}
+}
+
+func BenchmarkRadial_DisanceSquared(b *testing.B) {
+	var data []geo.LineString
+	ls := benchmarkData()
+	for i := 0; i < b.N; i++ {
+		data = append(data, ls.Clone())
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Radial(planar.DistanceSquared, 0.1*0.1).LineString(data[i])
 	}
 }
 
