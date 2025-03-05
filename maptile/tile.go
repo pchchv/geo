6619ec16cf40deb933feb5872ab5f434a800ceb5
pchchv <ipchchv@gmail.com>
2025-03-05 11:00:48 +0300

    Add Tile type and constructor for web mercator tiles

--- maptile/tile.go
+++ maptile/tile.go
@@ -2,3 +2,15 @@ package maptile
 
 // Zoom is a strict type for a tile zoom level.
 type Zoom uint32
+
+// Tile is an x, y, z web mercator tile.
+type Tile struct {
+	X uint32
+	Y uint32
+	Z Zoom
+}
+
+// New creates a new tile with the given coordinates.
+func New(x, y uint32, z Zoom) Tile {
+	return Tile{x, y, z}
+}
