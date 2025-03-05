8aa017590f29cae5ad1aa97418333cedda7d3577
pchchv <ipchchv@gmail.com>
2025-03-05 10:13:46 +0300

    Add benchmarks for Douglas Peucker simplification algorithm

--- simplify/benchmarks_test.go
+++ simplify/benchmarks_test.go
@@ -3,9 +3,48 @@ package simplify
 import (
 	"encoding/json"
 	"os"
+	"testing"
 
 	"github.com/pchchv/geo"
 )
+
+func TestDouglasPeucker_BenchmarkData(t *testing.T) {
+	cases := []struct {
+		threshold float64
+		length    int
+	}{
+		{0.1, 1118},
+		{0.5, 257},
+		{1.0, 144},
+		{1.5, 95},
+		{2.0, 71},
+		{3.0, 46},
+		{4.0, 39},
+		{5.0, 33},
+	}
+
+	ls := benchmarkData()
+	for i, tc := range cases {
+		r := DouglasPeucker(tc.threshold).LineString(ls.Clone())
+		if len(r) != tc.length {
+			t.Errorf("%d: reduced poorly, %d != %d", i, len(r), tc.length)
+		}
+	}
+}
+
+func BenchmarkDouglasPeucker(b *testing.B) {
+	var data []geo.LineString
+	ls := benchmarkData()
+	for i := 0; i < b.N; i++ {
+		data = append(data, ls.Clone())
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		DouglasPeucker(0.1).LineString(data[i])
+	}
+}
 
 func benchmarkData() (ls geo.LineString) {
 	f, err := os.Open("testdata/lisbon2portugal.json")
