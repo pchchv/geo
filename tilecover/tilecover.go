d5936b01286ce1e2c26a3e55344478308c223727
pchchv <ipchchv@gmail.com>
2025-03-06 06:00:07 +0300

    Add Bound function to create tile cover for specified bounds

--- tilecover/tilecover.go
+++ tilecover/tilecover.go
@@ -22,3 +22,18 @@ func MultiPoint(mp geo.MultiPoint, z maptile.Zoom) maptile.Set {
 
 	return set
 }
+
+// Bound creates a tile cover for the bound. i.e. all the tiles
+// that intersect the bound.
+func Bound(b geo.Bound, z maptile.Zoom) maptile.Set {
+	lo := maptile.At(b.Min, z)
+	hi := maptile.At(b.Max, z)
+	result := make(maptile.Set, (hi.X-lo.X+1)*(lo.Y-hi.Y+1))
+	for x := lo.X; x <= hi.X; x++ {
+		for y := hi.Y; y <= lo.Y; y++ {
+			result[maptile.Tile{X: x, Y: y, Z: z}] = true
+		}
+	}
+
+	return result
+}
