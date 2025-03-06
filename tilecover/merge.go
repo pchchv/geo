b472a36264e8539c2ee267ffd0d712e27cd5a69f
pchchv <ipchchv@gmail.com>
2025-03-06 09:55:14 +0300

    Add MergeUpPartial function to merge tiles based on sibling count

--- tilecover/merge.go
+++ tilecover/merge.go
@@ -77,3 +77,96 @@ func MergeUp(set maptile.Set, min maptile.Zoom) maptile.Set {
 
 	return merged
 }
+
+// MergeUpPartial will merge tiles in the given set to the given min zoom.
+// Tiles will be merged if the set has `count` of siblings.
+// It is assumed that all tiles in the input set will have the same zoom,
+// e.g. the outputs of the Geometry function.
+func MergeUpPartial(set maptile.Set, min maptile.Zoom, count int) maptile.Set {
+	max := maptile.Zoom(1)
+	for t, v := range set {
+		if v {
+			max = t.Z
+			break
+		}
+	}
+
+	if min == max {
+		return set
+	}
+
+	merged := make(maptile.Set)
+	for z := max; z > min; z-- {
+		parentSet := make(maptile.Set)
+		for t, v := range set {
+			if !v {
+				continue
+			}
+
+			var c int
+			sibs := t.Siblings()
+			s0 := set[sibs[0]]
+			s1 := set[sibs[1]]
+			s2 := set[sibs[2]]
+			s3 := set[sibs[3]]
+			if s0 {
+				c++
+			}
+
+			if s1 {
+				c++
+			}
+
+			if s2 {
+				c++
+			}
+
+			if s3 {
+				c++
+			}
+
+			if c >= count {
+				set[sibs[0]] = false
+				set[sibs[1]] = false
+				set[sibs[2]] = false
+				set[sibs[3]] = false
+				parent := t.Parent()
+				if z-1 == min {
+					merged[parent] = true
+				} else {
+					parentSet[parent] = true
+				}
+			} else {
+				if s0 {
+					merged[sibs[0]] = true
+					set[sibs[0]] = false
+				}
+
+				if s1 {
+					merged[sibs[1]] = true
+					set[sibs[1]] = false
+				}
+
+				if s2 {
+					merged[sibs[2]] = true
+					set[sibs[2]] = false
+				}
+
+				if s3 {
+					merged[sibs[3]] = true
+					set[sibs[3]] = false
+				}
+			}
+		}
+
+		set = parentSet
+		if len(set) < count {
+			for t := range set {
+				merged[t] = true
+			}
+			break
+		}
+	}
+
+	return merged
+}
