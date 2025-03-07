b1e040704523866001f6c97ef6cc212d3f78eed2
pchchv <ipchchv@gmail.com>
2025-03-07 10:33:48 +0300

    Add sorting and output utilities for GeoJSON feature collections in tests

--- tilecover/cover_test.go
+++ tilecover/cover_test.go
@@ -1,9 +1,12 @@
 package tilecover
 
 import (
+	"encoding/json"
 	"os"
+	"sort"
 	"testing"
 
+	"github.com/pchchv/geo"
 	"github.com/pchchv/geo/geojson"
 )
 
@@ -55,4 +58,38 @@ func loadFeatureCollection(t testing.TB, path string) *geojson.FeatureCollection
 
 	fc.Features = fc.Features[:count]
 	return fc
+}
+
+func sortFC(fc *geojson.FeatureCollection) {
+	sort.Slice(fc.Features, func(i, j int) bool {
+		a := fc.Features[i].Geometry.(geo.Polygon)[0]
+		b := fc.Features[j].Geometry.(geo.Polygon)[0]
+		if a[0][0] != b[0][0] {
+			return a[0][0] < b[0][0]
+		}
+
+		return a[0][1] < b[0][1]
+	})
+}
+
+// output gets called if there is a test failure for debugging.
+func output(t testing.TB, name string, r *geojson.FeatureCollection) {
+	f := loadFeature(t, "./testdata/"+name+".geojson")
+	if f.Properties == nil {
+		f.Properties = make(geojson.Properties)
+	}
+
+	f.Properties["fill"] = "#FF0000"
+	f.Properties["fill-opacity"] = "0.5"
+	f.Properties["stroke"] = "#FF0000"
+	f.Properties["name"] = "original"
+	r.Append(f)
+	data, err := json.MarshalIndent(r, "", " ")
+	if err != nil {
+		t.Fatalf("error marshalling json: %v", err)
+	}
+
+	if err = os.WriteFile("failure_"+name+".geojson", data, 0644); err != nil {
+		t.Fatalf("write file failure: %v", err)
+	}
 }
