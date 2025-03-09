3e012a1aa677c57443b0fb07c65d78b9fc510941
pchchv <ipchchv@gmail.com>
2025-03-09 09:08:19 +0300

    Implement BoundHeight and BoundWidth functions for calculating dimensions of geo bounds

--- geometries/bound.go
+++ geometries/bound.go
@@ -1,6 +1,24 @@
 package geometries
 
-import "math"
+import (
+	"math"
+
+	"github.com/pchchv/geo"
+)
+
+// BoundHeight returns the approximate height in meters.
+func BoundHeight(b geo.Bound) float64 {
+	return 111131.75 * (b.Max[1] - b.Min[1])
+}
+
+// BoundWidth returns the approximate width in meters
+// of the center of the bound.
+func BoundWidth(b geo.Bound) float64 {
+	c := (b.Min[1] + b.Max[1]) / 2.0
+	s1 := geo.Point{b.Min[0], c}
+	s2 := geo.Point{b.Max[0], c}
+	return Distance(s1, s2)
+}
 
 func deg2rad(d float64) float64 {
 	return d * math.Pi / 180.0
