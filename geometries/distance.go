ece3ff5fe8a513b15b9664192239732d175bcb98
pchchv <ipchchv@gmail.com>
2025-03-09 09:04:55 +0300

    Add midpoint and point-at-bearing functions for geographic calculations

--- geometries/distance.go
+++ geometries/distance.go
@@ -30,3 +30,33 @@ func DistanceHaversine(p1, p2 geo.Point) float64 {
 
 	return 2.0 * geo.EarthRadius * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 }
+
+// Midpoint returns the half-way point along a great circle path between the two points.
+func Midpoint(p, p2 geo.Point) geo.Point {
+	dLon := deg2rad(p2[0] - p[0])
+	aLatRad := deg2rad(p[1])
+	bLatRad := deg2rad(p2[1])
+	x := math.Cos(bLatRad) * math.Cos(dLon)
+	y := math.Cos(bLatRad) * math.Sin(dLon)
+	r := geo.Point{
+		deg2rad(p[0]) + math.Atan2(y, math.Cos(aLatRad)+x),
+		math.Atan2(math.Sin(aLatRad)+math.Sin(bLatRad), math.Sqrt((math.Cos(aLatRad)+x)*(math.Cos(aLatRad)+x)+y*y)),
+	}
+
+	// convert back to degrees
+	r[0] = rad2deg(r[0])
+	r[1] = rad2deg(r[1])
+
+	return r
+}
+
+// PointAtBearingAndDistance returns the point at the given bearing and distance in meters from the point
+func PointAtBearingAndDistance(p geo.Point, bearing, distance float64) geo.Point {
+	aLat := deg2rad(p[1])
+	aLon := deg2rad(p[0])
+	bearingRadians := deg2rad(bearing)
+	distanceRatio := distance / geo.EarthRadius
+	bLat := math.Asin(math.Sin(aLat)*math.Cos(distanceRatio) + math.Cos(aLat)*math.Sin(distanceRatio)*math.Cos(bearingRadians))
+	bLon := aLon + math.Atan2(math.Sin(bearingRadians)*math.Sin(distanceRatio)*math.Cos(aLat), math.Cos(distanceRatio)-math.Sin(aLat)*math.Sin(bLat))
+	return geo.Point{rad2deg(bLon), rad2deg(bLat)}
+}
