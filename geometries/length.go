9d82168919701cca2020cec169d10b2e501bd273
pchchv <ipchchv@gmail.com>
2025-03-10 09:58:25 +0300

    Implement LengthHaversine func to calculate geometry boundary length using haversine formula

--- geometries/length.go
+++ geometries/length.go
@@ -5,9 +5,12 @@ import (
 	"github.com/pchchv/geo/internal/length"
 )
 
-// Length returns the length of the
-// boundary of the geometry using the
-// geo distance function.
+// Length returns the length of the boundary of the geometry using the geo distance function.
 func Length(g geo.Geometry) float64 {
 	return length.Length(g, Distance)
+}
+
+// LengthHaversine returns the length of the boundary of the geometry using the geo haversine formula.
+func LengthHaversine(g geo.Geometry) float64 {
+	return length.Length(g, DistanceHaversine)
 }
