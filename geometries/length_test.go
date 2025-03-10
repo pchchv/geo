380c0602be26bf429ca8d450061e05586af72efb
pchchv <ipchchv@gmail.com>
2025-03-10 09:56:19 +0300

    Add unit test for Length function to ensure it handles all geometry types without panic

---
+++ geometries/length_test.go
@@ -0,0 +1,14 @@
+package geometries
+
+import (
+	"testing"
+
+	"github.com/pchchv/geo"
+)
+
+func TestLength(t *testing.T) {
+	for _, g := range geo.AllGeometries {
+		// should not panic with unsupported type
+		Length(g)
+	}
+}
