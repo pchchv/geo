e0794fffb0cef60f5d18fdc9785d8f87b36c71f3
pchchv <ipchchv@gmail.com>
2025-03-11 09:29:42 +0300

    Implement RemoveEmpty method to filter features by geometry size limits

--- encoding/mvt/simplify.go
+++ encoding/mvt/simplify.go
@@ -1,14 +1,9 @@
 package mvt
 
-import "github.com/pchchv/geo"
-
-// Simplify will run all the geometry of all the
-// layers through the provided simplifer.
-func (ls Layers) Simplify(s geo.Simplifier) {
-	for _, l := range ls {
-		l.Simplify(s)
-	}
-}
+import (
+	"github.com/pchchv/geo"
+	"github.com/pchchv/geo/planar"
+)
 
 // Simplify will run the layer geometries through the simplifier.
 func (l *Layer) Simplify(s geo.Simplifier) {
@@ -25,4 +20,48 @@ func (l *Layer) Simplify(s geo.Simplifier) {
 	}
 
 	l.Features = l.Features[:count]
+}
+
+// RemoveEmpty will remove line strings shorter/smaller than the limits.
+func (l *Layer) RemoveEmpty(lineLimit, areaLimit float64) {
+	var count int
+	for i := 0; i < len(l.Features); i++ {
+		f := l.Features[i]
+		if f.Geometry == nil {
+			continue
+		}
+
+		switch f.Geometry.Dimensions() {
+		case 0: // point geometry
+			l.Features[count] = f
+			count++
+		case 1: // line geometry
+			if planar.Length(f.Geometry) >= lineLimit {
+				l.Features[count] = f
+				count++
+			}
+		case 2:
+			if planar.Area(f.Geometry) >= areaLimit {
+				l.Features[count] = f
+				count++
+			}
+		}
+	}
+
+	l.Features = l.Features[:count]
+}
+
+// Simplify will run all the geometry of all the
+// layers through the provided simplifer.
+func (ls Layers) Simplify(s geo.Simplifier) {
+	for _, l := range ls {
+		l.Simplify(s)
+	}
+}
+
+// RemoveEmpty will remove line strings shorter/smaller than the limits.
+func (ls Layers) RemoveEmpty(lineLimit, areaLimit float64) {
+	for _, l := range ls {
+		l.RemoveEmpty(lineLimit, areaLimit)
+	}
 }
