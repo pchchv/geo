63ce11d341401e2b00d29bec79775ffcee8bbae3
pchchv <ipchchv@gmail.com>
2025-03-11 09:20:46 +0300

    Implement newProjection function for handling tile projections based on extent

--- encoding/mvt/projection.go
+++ encoding/mvt/projection.go
@@ -2,6 +2,7 @@ package mvt
 
 import (
 	"math"
+	"math/bits"
 
 	"github.com/pchchv/geo"
 	"github.com/pchchv/geo/internal/mercator"
@@ -11,6 +12,31 @@ import (
 type projection struct {
 	ToTile  geo.Projection
 	ToWGS84 geo.Projection
+}
+
+func newProjection(tile maptile.Tile, extent uint32) *projection {
+	if isPowerOfTwo(extent) {
+		// powers of two extents allows for some more simplicity
+		n := uint32(bits.TrailingZeros32(extent))
+		z := uint32(tile.Z) + n
+		minx, miny := float64(uint64(tile.X)<<n), float64(uint64(tile.Y)<<n)
+
+		return &projection{
+			ToTile: func(p geo.Point) geo.Point {
+				x, y := mercator.ToPlanar(p[0], p[1], z)
+				return geo.Point{
+					math.Floor(x - minx),
+					math.Floor(y - miny),
+				}
+			},
+			ToWGS84: func(p geo.Point) geo.Point {
+				lon, lat := mercator.ToGeo(p[0]+minx+0.5, p[1]+miny+0.5, z)
+				return geo.Point{lon, lat}
+			},
+		}
+	}
+
+	return nonPowerOfTwoProjection(tile, extent)
 }
 
 func isPowerOfTwo(n uint32) bool {
