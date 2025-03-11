3a7defeb352eace0b3ba7e86e32623a9d2a721e9
pchchv <ipchchv@gmail.com>
2025-03-11 09:14:42 +0300

    Add Layer type and NewLayer helper for MVT encoding

--- encoding/mvt/layer.go
+++ encoding/mvt/layer.go
@@ -1,1 +1,24 @@
 package mvt
+
+import "github.com/pchchv/geo/geojson"
+
+const DefaultExtent = 4096
+
+// Layer is intermediate MVT layer to be encoded/decoded or projected.
+type Layer struct {
+	Name     string
+	Version  uint32
+	Extent   uint32
+	Features []*geojson.Feature
+}
+
+// NewLayer is a helper to create a Layer from a feature collection and a name,
+// it sets the default extent and version to 1.
+func NewLayer(name string, fc *geojson.FeatureCollection) *Layer {
+	return &Layer{
+		Name:     name,
+		Version:  1,
+		Extent:   DefaultExtent,
+		Features: fc.Features,
+	}
+}
