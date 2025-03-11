f784fa622770c300b1713fcbe82ec7887b587c9f
pchchv <ipchchv@gmail.com>
2025-03-11 09:30:17 +0300

    Add unit tests for RemoveEmpty method in Layer

--- encoding/mvt/simplify_test.go
+++ encoding/mvt/simplify_test.go
@@ -27,3 +27,34 @@ func TestLayerSimplify(t *testing.T) {
 		t.Errorf("incorrect type: %v", v)
 	}
 }
+
+func TestLayerRemoveEmpty(t *testing.T) {
+	// should remove empty features
+	ls := Layers{&Layer{
+		Features: []*geojson.Feature{
+			geojson.NewFeature(geo.Ring{{0, 0}, {1, 1}, {0, 1}, {0, 0}}),
+			geojson.NewFeature(geo.LineString{{0, 0}, {5, 5}, {0, 0}}),
+		},
+	}}
+
+	ls.RemoveEmpty(2, 0.5)
+	if len(ls[0].Features) != 2 {
+		t.Errorf("should not remove things above the limit")
+	}
+
+	// remove the area
+	ls.RemoveEmpty(2, 15)
+	if len(ls[0].Features) != 1 {
+		t.Errorf("should remove empty feature")
+	}
+
+	if v := ls[0].Features[0].Geometry.GeoJSONType(); v != "LineString" {
+		t.Errorf("incorrect type: %v", v)
+	}
+
+	// remove the line
+	ls.RemoveEmpty(15, 2)
+	if len(ls[0].Features) != 0 {
+		t.Errorf("should remove empty feature")
+	}
+}
