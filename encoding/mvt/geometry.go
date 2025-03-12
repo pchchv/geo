a8233d202f7b7d8862d931d49dbaba508f99d873
pchchv <ipchchv@gmail.com>
2025-03-12 09:59:07 +0300

    Implement encodeValue function

--- encoding/mvt/geometry.go
+++ encoding/mvt/geometry.go
@@ -1,6 +1,10 @@
 package mvt
 
-import "github.com/pchchv/geo/encoding/mvt/vectortile"
+import (
+	"fmt"
+
+	"github.com/pchchv/geo/encoding/mvt/vectortile"
+)
 
 type keyValueEncoder struct {
 	Keys          []string
@@ -27,3 +31,53 @@ func (kve *keyValueEncoder) Key(s string) (i uint32) {
 	return
 }
 
+func encodeValue(v interface{}) (*vectortile.Tile_Value, error) {
+	tv := &vectortile.Tile_Value{}
+	switch t := v.(type) {
+	case string:
+		tv.StringValue = &t
+	case fmt.Stringer:
+		s := t.String()
+		tv.StringValue = &s
+	case int:
+		i := int64(t)
+		tv.SintValue = &i
+	case int8:
+		i := int64(t)
+		tv.SintValue = &i
+	case int16:
+		i := int64(t)
+		tv.SintValue = &i
+	case int32:
+		i := int64(t)
+		tv.SintValue = &i
+	case int64:
+		i := int64(t)
+		tv.SintValue = &i
+	case uint:
+		i := uint64(t)
+		tv.UintValue = &i
+	case uint8:
+		i := uint64(t)
+		tv.UintValue = &i
+	case uint16:
+		i := uint64(t)
+		tv.UintValue = &i
+	case uint32:
+		i := uint64(t)
+		tv.UintValue = &i
+	case uint64:
+		i := uint64(t)
+		tv.UintValue = &i
+	case float32:
+		tv.FloatValue = &t
+	case float64:
+		tv.DoubleValue = &t
+	case bool:
+		tv.BoolValue = &t
+	default:
+		return nil, fmt.Errorf("unable to encode value of type %T: %v", v, v)
+	}
+
+	return tv, nil
+}
