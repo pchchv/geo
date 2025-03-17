8d8db4373dd45150c6a416031a5aff1a1624306f
pchchv <ipchchv@gmail.com>
2025-03-17 11:03:38 +0300

    Impelemt a NextPoint method

--- encoding/mvt/unmarshal.go
+++ encoding/mvt/unmarshal.go
@@ -21,3 +21,20 @@ type geomDecoder struct {
 	used  int
 	prev  geo.Point
 }
+
+func (gd *geomDecoder) NextPoint() (geo.Point, error) {
+	gd.used += 2
+	v, err := gd.iter.Uint32()
+	if err != nil {
+		return geo.Point{}, err
+	}
+	gd.prev[0] += unzigzag(v)
+
+	v, err = gd.iter.Uint32()
+	if err != nil {
+		return geo.Point{}, err
+	}
+	gd.prev[1] += unzigzag(v)
+
+	return gd.prev, nil
+}
