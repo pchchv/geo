8967f2e0d4b2fadab7447df5954394c37e5cc0cd
pchchv <ipchchv@gmail.com>
2025-03-17 10:49:36 +0300

    Add marshaling unit tests

--- encoding/mvt/marshal_test.go
+++ encoding/mvt/marshal_test.go
@@ -1,6 +1,7 @@
 package mvt
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"math"
@@ -12,6 +13,423 @@ import (
 	"github.com/pchchv/geo/geojson"
 	"github.com/pchchv/geo/maptile"
 )
+
+func TestMarshalUnmarshalGzipped_Full(t *testing.T) {
+	tile := maptile.New(8956, 12223, 15)
+	ls := geo.LineString{
+		{-81.60346275, 41.50998572},
+		{-81.6033669, 41.50991259},
+		{-81.60355599, 41.50976036},
+		{-81.6040648, 41.50936811},
+		{-81.60404411, 41.50935405},
+	}
+	expected := ls.Clone()
+
+	f := geojson.NewFeature(ls)
+	f.Properties = geojson.Properties{
+		"source":       "openstreetmap.org",
+		"kind":         "path",
+		"name":         "Uptown Alley",
+		"landuse_kind": "retail",
+		"sort_rank":    float64(354),
+		"kind_detail":  "pedestrian",
+		"min_zoom":     float64(13),
+		"id":           float64(246698394),
+	}
+
+	fc := geojson.NewFeatureCollection()
+	fc.Append(f)
+
+	layers := Layers{NewLayer("roads", fc)}
+
+	// project to the tile coords
+	layers.ProjectToTile(tile)
+
+	// marshal
+	encoded, err := MarshalGzipped(layers)
+	if err != nil {
+		t.Fatalf("marshal error: %e", err)
+	}
+
+	// unmarshal
+	decoded, err := UnmarshalGzipped(encoded)
+	if err != nil {
+		t.Fatalf("unmarshal error: %e", err)
+	}
+
+	// project back
+	decoded.ProjectToWGS84(tile)
+
+	// compare the results
+	result := decoded[0].Features[0]
+	compareProperties(t, result.Properties, f.Properties)
+
+	// compare geometry
+	xe, ye := tileEpsilon(tile)
+	compareGEOGeometry(t, result.Geometry, expected, xe, ye)
+}
+
+func TestMarshalUnmarshalForGeometryCollection(t *testing.T) {
+	tile := maptile.New(8956, 12223, 15)
+	outerRing := geo.Ring{
+		{-81.6033000, 41.5099000},
+		{-81.6033000, 41.5094000},
+		{-81.6039000, 41.5094000},
+		{-81.6039000, 41.5099000},
+		{-81.6033000, 41.5099000},
+	}
+	hole := geo.Ring{
+		{-81.60389989614487, 41.50941085679876},
+		{-81.60329908132553, 41.50941085679876},
+		{-81.60329908132553, 41.50990496161759},
+		{-81.60389989614487, 41.50990496161759},
+		{-81.60389989614487, 41.50941085679876},
+	}
+	outerPolygon := geo.Polygon{outerRing, hole}
+	polygonInHole := geo.Polygon{geo.Ring{
+		{-81.60375505685806, 41.5095494475553},
+		{-81.6034385561943, 41.5095494475553},
+		{-81.6034385561943, 41.50978043149024},
+		{-81.60375505685806, 41.50978043149024},
+		{-81.60375505685806, 41.5095494475553},
+	}}
+	geometryCollection := geo.Collection{outerPolygon, polygonInHole}
+	expected := []geo.Geometry{outerPolygon.Clone(), outerPolygon.Clone()}
+
+	f := geojson.NewFeature(geometryCollection)
+	f.Properties = geojson.Properties{
+		"id": float64(246698394),
+	}
+
+	fc := geojson.NewFeatureCollection()
+	fc.Append(f)
+	layers := Layers{NewLayer("roads", fc)}
+
+	// project to the tile coords
+	layers.ProjectToTile(tile)
+
+	// marshal
+	encoded, err := MarshalGzipped(layers)
+	if err != nil {
+		t.Fatalf("marshal error: %e", err)
+	}
+
+	// unmarshal
+	decoded, err := UnmarshalGzipped(encoded)
+	if err != nil {
+		t.Fatalf("unmarshal error: %e", err)
+	}
+
+	// project back
+	decoded.ProjectToWGS84(tile)
+
+	// compare the results
+	results := decoded[0].Features
+	compareProperties(t, results[0].Properties, f.Properties)
+
+	// compare geometry
+	xe, ye := tileEpsilon(tile)
+	if len(results) == len(expected) {
+		t.Errorf("result geometry count must be splited polygon: %v (but result is %v)", len(results), len(expected))
+	}
+
+	for i, result := range results {
+		compareGEOGeometry(t, result.Geometry, expected[i], xe, ye)
+	}
+}
+
+func TestMarshal_nilGeometry(t *testing.T) {
+	// should handle nil geometry,
+	// possibly caused by clipping or other issues.
+	fc := geojson.NewFeatureCollection()
+	fc.Append(geojson.NewFeature(nil))
+	layers := Layers{NewLayer("roads", fc)}
+
+	// project to the tile coords
+	tile := maptile.New(0, 0, 15)
+	layers.ProjectToTile(tile)
+
+	// marshal
+	encoded, err := MarshalGzipped(layers)
+	if err != nil {
+		t.Fatalf("marshal error: %e", err)
+	}
+
+	// unmarshal
+	decoded, err := UnmarshalGzipped(encoded)
+	if err != nil {
+		t.Fatalf("unmarshal error: %e", err)
+	}
+
+	// project back
+	decoded.ProjectToWGS84(tile)
+	if v := len(decoded[0].Features); v != 0 {
+		t.Errorf("should have have any features, has %v", v)
+	}
+}
+
+func TestMarshalUnmarshal(t *testing.T) {
+	cases := []struct {
+		name string
+		tile maptile.Tile
+	}{
+		{
+			name: "15-8956-12223",
+			tile: maptile.New(8956, 12223, 15),
+		},
+		{
+			name: "16-17896-24449",
+			tile: maptile.New(17896, 24449, 16),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			expected := loadGeoJSON(t, tc.tile)
+			layers := NewLayers(loadGeoJSON(t, tc.tile))
+			layers.ProjectToTile(tc.tile)
+			data, err := Marshal(layers)
+			if err != nil {
+				t.Errorf("marshal error: %e", err)
+			}
+
+			layers, err = Unmarshal(data)
+			if err != nil {
+				t.Errorf("unmarshal error: %e", err)
+			}
+
+			layers.ProjectToWGS84(tc.tile)
+			result := layers.ToFeatureCollections()
+			xEpsilon, yEpsilon := tileEpsilon(tc.tile)
+			for key := range expected {
+				for i := range expected[key].Features {
+					r := result[key].Features[i]
+					e := expected[key].Features[i]
+
+					// t.Logf("checking layer %s: feature %d", key, i)
+					compareProperties(t, r.Properties, e.Properties)
+					compareGEOGeometry(t, r.Geometry, e.Geometry, xEpsilon, yEpsilon)
+				}
+			}
+		})
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	cases := []struct {
+		name string
+		tile maptile.Tile
+	}{
+		{
+			name: "15-8956-12223",
+			tile: maptile.New(8956, 12223, 15),
+		},
+		{
+			name: "16-17896-24449",
+			tile: maptile.New(17896, 24449, 16),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			expected := loadGeoJSON(t, tc.tile)
+			layers, err := UnmarshalGzipped(loadMVT(t, tc.tile))
+			if err != nil {
+				t.Fatalf("error unmarshalling: %e", err)
+			}
+
+			layers.ProjectToWGS84(tc.tile)
+			result := layers.ToFeatureCollections()
+			xEpsilon, yEpsilon := tileEpsilon(tc.tile)
+			for key := range expected {
+				for i := range expected[key].Features {
+					r := result[key].Features[i]
+					e := expected[key].Features[i]
+					t.Logf("checking layer %s: feature %d", key, i)
+					compareProperties(t, r.Properties, e.Properties)
+					compareGEOGeometry(t, r.Geometry, e.Geometry, xEpsilon, yEpsilon)
+				}
+			}
+		})
+	}
+}
+
+func TestUnmarshalGzippedTileWithRegularUnmarshalFunction(t *testing.T) {
+	t.Run("15-8956-12223", func(t *testing.T) {
+		tile := maptile.New(8956, 12223, 15)
+		if _, err := Unmarshal(loadMVT(t, tile)); err != ErrDataIsGZipped {
+			t.Fatal()
+		}
+	})
+}
+
+func TestMarshal_ID(t *testing.T) {
+	cases := []struct {
+		name string
+		id   interface{}
+		val  float64
+	}{
+		{
+			name: "int",
+			id:   int(86427531),
+			val:  86427531,
+		},
+		{
+			name: "int8",
+			id:   int8(123),
+			val:  123,
+		},
+		{
+			name: "int16",
+			id:   int16(6884),
+			val:  6884,
+		},
+		{
+			name: "int32",
+			id:   int32(123),
+			val:  123,
+		},
+		{
+			name: "int64",
+			id:   int64(12345678),
+			val:  12345678,
+		},
+		{
+			name: "uint",
+			id:   uint(86427531),
+			val:  86427531,
+		},
+		{
+			name: "uint8",
+			id:   uint8(123),
+			val:  123,
+		},
+		{
+			name: "uint16",
+			id:   uint16(6884),
+			val:  6884,
+		},
+		{
+			name: "uint32",
+			id:   uint32(123),
+			val:  123,
+		},
+		{
+			name: "uint64",
+			id:   uint64(12345678),
+			val:  12345678,
+		},
+		{
+			name: "float32",
+			id:   float32(123.45),
+			val:  123,
+		},
+		{
+			name: "float64",
+			id:   float64(123.45),
+			val:  123,
+		},
+		{
+			name: "string",
+			id:   "123456",
+			val:  123456,
+		},
+
+		// negatives
+		{
+			name: "negative string",
+			id:   "-123456",
+			val:  0, // nil
+		},
+		{
+			name: "negative int",
+			id:   int(-123456),
+			val:  0, // nil
+		},
+		{
+			name: "negative int64",
+			id:   int64(-123456),
+			val:  0, // nil
+		},
+		{
+			name: "negative float64",
+			id:   float64(-123456),
+			val:  0, // nil
+		},
+	}
+
+	f := geojson.NewFeature(geo.Point{1, 2})
+	f.Properties["type"] = "point"
+	fc := geojson.NewFeatureCollection().Append(f)
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			f.ID = tc.id
+			data, err := Marshal(NewLayers(map[string]*geojson.FeatureCollection{"roads": fc}))
+			if err != nil {
+				t.Errorf("marshal error: %e", err)
+			}
+
+			ls, err := Unmarshal(data)
+			if err != nil {
+				t.Errorf("unmarshal error: %e", err)
+			}
+
+			id := ls.ToFeatureCollections()["roads"].Features[0].ID
+			if tc.val > 0 {
+				if id.(float64) != tc.val {
+					t.Errorf("incorrect id: %v != %v", id, tc.val)
+				}
+			} else {
+				if id != nil {
+					t.Errorf("id should be nil: %v", id)
+				}
+			}
+		})
+	}
+
+	t.Run("unmarshaled int from json", func(t *testing.T) {
+		f.ID = 123
+		data, err := fc.MarshalJSON()
+		if err != nil {
+			t.Fatalf("json marshal error: %e", err)
+		}
+
+		fc, err = geojson.UnmarshalFeatureCollection(data)
+		if err != nil {
+			t.Fatalf("unmarshal json error: %e", err)
+		}
+
+		if _, ok := fc.Features[0].ID.(float64); !ok {
+			t.Errorf("json should unmarshal numbers to float64: %T", fc.Features[0].ID)
+		}
+
+		data, err = Marshal(NewLayers(map[string]*geojson.FeatureCollection{"roads": fc}))
+		if err != nil {
+			t.Errorf("marshal error: %e", err)
+		}
+
+		ls, err := Unmarshal(data)
+		if err != nil {
+			t.Errorf("unmarshal error: %e", err)
+		}
+
+		id := ls.ToFeatureCollections()["roads"].Features[0].ID
+		if _, ok := id.(float64); !ok {
+			t.Errorf("should unmarshal id to float64: %T", id)
+		}
+	})
+}
+
+func TestStableMarshalling(t *testing.T) {
+	layers := NewLayers(loadGeoJSON(t, maptile.New(17896, 24449, 16)))
+	firstData, _ := Marshal(layers)
+	for i := 0; i < 100; i++ {
+		data, _ := Marshal(layers)
+		if !bytes.Equal(data, firstData) {
+			t.Errorf("a marshal had different bytes")
+		}
+	}
+}
 
 func compareProperties(t testing.TB, result, expected geojson.Properties) {
 	t.Helper()
