851ae9e11dd87255793b7330a60a2251179950ce
pchchv <ipchchv@gmail.com>
2025-03-17 10:51:31 +0300

    Add unit tests for nonPowerOfTwoProjection functionality

--- encoding/mvt/projection_test.go
+++ encoding/mvt/projection_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/pchchv/geo/maptile"
+	"github.com/pchchv/geo/project"
 )
 
 const epsilon = 1e-6
@@ -30,5 +31,48 @@ func TestPowerOfTwoProjection(t *testing.T) {
 
 	if math.Abs(geo[1]-center[1]) > epsilon {
 		t.Errorf("lat miss match: %f != %f", geo[1], center[1])
+	}
+}
+
+func TestNonPowerOfTwoProjection(t *testing.T) {
+	tile := maptile.New(8956, 12223, 15)
+	regProj := newProjection(tile, 4096)
+	nonProj := nonPowerOfTwoProjection(tile, 4096)
+	expected := loadGeoJSON(t, tile)
+	layers := NewLayers(loadGeoJSON(t, tile))
+	// loopy de loop of projections
+	for _, l := range layers {
+		for _, f := range l.Features {
+			f.Geometry = project.Geometry(f.Geometry, regProj.ToTile)
+		}
+	}
+
+	for _, l := range layers {
+		for _, f := range l.Features {
+			f.Geometry = project.Geometry(f.Geometry, nonProj.ToWGS84)
+		}
+	}
+
+	for _, l := range layers {
+		for _, f := range l.Features {
+			f.Geometry = project.Geometry(f.Geometry, nonProj.ToTile)
+		}
+	}
+
+	for _, l := range layers {
+		for _, f := range l.Features {
+			f.Geometry = project.Geometry(f.Geometry, regProj.ToWGS84)
+		}
+	}
+
+	result := layers.ToFeatureCollections()
+	xEpsilon, yEpsilon := tileEpsilon(tile)
+	for key := range expected {
+		for i := range expected[key].Features {
+			r := result[key].Features[i]
+			e := expected[key].Features[i]
+
+			compareGEOGeometry(t, r.Geometry, e.Geometry, xEpsilon, yEpsilon)
+		}
 	}
 }
