b2830724182018123f418307e3ad4a012e6eb76f
pchchv <ipchchv@gmail.com>
2025-03-18 10:16:49 +0300

    Implement a readCollection function

--- encoding/wkbcommon/collection.go
+++ encoding/wkbcommon/collection.go
@@ -1,6 +1,10 @@
 package wkbcommon
 
-import "github.com/pchchv/geo"
+import (
+	"io"
+
+	"github.com/pchchv/geo"
+)
 
 func (e *Encoder) writeCollection(c geo.Collection, srid int) (err error) {
 	if err = e.writeTypePrefix(geometryCollectionType, len(c), srid); err != nil {
@@ -14,4 +18,30 @@ func (e *Encoder) writeCollection(c geo.Collection, srid int) (err error) {
 	}
 
 	return nil
+}
+
+func readCollection(r io.Reader, order byteOrder, buf []byte) (geo.Collection, error) {
+	num, err := readUint32(r, order, buf[:4])
+	if err != nil {
+		return nil, err
+	}
+
+	alloc := num
+	if alloc > MaxMultiAlloc {
+		// invalid data can come in here and allocate tons of memory.
+		alloc = MaxMultiAlloc
+	}
+	result := make(geo.Collection, 0, alloc)
+
+	d := NewDecoder(r)
+	for i := 0; i < int(num); i++ {
+		geom, _, err := d.Decode()
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, geom)
+	}
+
+	return result, nil
 }
