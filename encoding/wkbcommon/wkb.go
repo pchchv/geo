35030ae156e1bd882d461b576eadde0a3139a2ac
pchchv <ipchchv@gmail.com>
2025-03-18 09:00:10 +0300

    Implement a constructor for the Encoder type

--- encoding/wkbcommon/wkb.go
+++ encoding/wkbcommon/wkb.go
@@ -5,10 +5,20 @@ import (
 	"io"
 )
 
+var DefaultByteOrder binary.ByteOrder = binary.LittleEndian // order used for marshalling or encoding
+
 // Encoder encodes a geometry as (E)WKB for the
 // writer specified at creation.
 type Encoder struct {
 	buf   []byte
 	w     io.Writer
 	order binary.ByteOrder
+}
+
+// NewEncoder creates a new Encoder for the given writer.
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{
+		w:     w,
+		order: DefaultByteOrder,
+	}
 }
