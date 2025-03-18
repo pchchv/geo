17c9cf012ac4693905cc601c18ade82af34a8f9c
pchchv <ipchchv@gmail.com>
2025-03-18 09:04:52 +0300

    Implement writeTypePrefix method

--- encoding/wkbcommon/wkb.go
+++ encoding/wkbcommon/wkb.go
@@ -5,6 +5,8 @@ import (
 	"io"
 )
 
+const ewkbType uint32 = 0x20000000
+
 var DefaultByteOrder binary.ByteOrder = binary.LittleEndian // order used for marshalling or encoding
 
 // Encoder encodes a geometry as (E)WKB for the
@@ -21,4 +23,19 @@ func NewEncoder(w io.Writer) *Encoder {
 		w:     w,
 		order: DefaultByteOrder,
 	}
+}
+
+func (e *Encoder) writeTypePrefix(t uint32, l int, srid int) error {
+	if srid == 0 {
+		e.order.PutUint32(e.buf, t)
+		e.order.PutUint32(e.buf[4:], uint32(l))
+		_, err := e.w.Write(e.buf[:8])
+		return err
+	}
+
+	e.order.PutUint32(e.buf, t|ewkbType)
+	e.order.PutUint32(e.buf[4:], uint32(srid))
+	e.order.PutUint32(e.buf[8:], uint32(l))
+	_, err := e.w.Write(e.buf[:12])
+	return err
 }
