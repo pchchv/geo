d6e45834db0f90dfa6ce302070c0ff405787aa6c
pchchv <ipchchv@gmail.com>
2025-03-18 10:16:08 +0300

    Implement a readPolygon and a readMultiPolygon functions

--- encoding/wkbcommon/polygon.go
+++ encoding/wkbcommon/polygon.go
@@ -42,3 +42,62 @@ func (e *Encoder) writeMultiPolygon(mp geo.MultiPolygon, srid int) (err error) {
 
 	return nil
 }
+
+func readPolygon(r io.Reader, order byteOrder, buf []byte) (geo.Polygon, error) {
+	num, err := readUint32(r, order, buf[:4])
+	if err != nil {
+		return nil, err
+	}
+
+	alloc := num
+	if alloc > MaxMultiAlloc {
+		// invalid data can come in here and allocate tons of memory.
+		alloc = MaxMultiAlloc
+	}
+	result := make(geo.Polygon, 0, alloc)
+
+	for i := 0; i < int(num); i++ {
+		ls, err := readLineString(r, order, buf)
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, geo.Ring(ls))
+	}
+
+	return result, nil
+}
+
+func readMultiPolygon(r io.Reader, order byteOrder, buf []byte) (geo.MultiPolygon, error) {
+	num, err := readUint32(r, order, buf[:4])
+	if err != nil {
+		return nil, err
+	}
+
+	alloc := num
+	if alloc > MaxMultiAlloc {
+		// invalid data can come in here and allocate tons of memory.
+		alloc = MaxMultiAlloc
+	}
+	result := make(geo.MultiPolygon, 0, alloc)
+
+	for i := 0; i < int(num); i++ {
+		pOrder, typ, _, err := readByteOrderType(r, buf)
+		if err != nil {
+			return nil, err
+		}
+
+		if typ != polygonType {
+			return nil, errors.New("expect multipolygons to contains polygons, did not find a polygon")
+		}
+
+		p, err := readPolygon(r, pOrder, buf)
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, p)
+	}
+
+	return result, nil
+}
