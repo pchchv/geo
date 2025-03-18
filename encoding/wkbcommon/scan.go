8613c0fe9fb7f13fbe160c5cf300b748bf512724
pchchv <ipchchv@gmail.com>
2025-03-18 10:26:54 +0300

    Implement a ScanPoint and a ScanMultiPoint functions

--- encoding/wkbcommon/scan.go
+++ encoding/wkbcommon/scan.go
@@ -1,6 +1,10 @@
 package wkbcommon
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/pchchv/geo"
+)
 
 var (
 	ErrNotWKB              = errors.New("wkbcommon: invalid data")         // returned when unmarshalling WKB and the data is not valid
@@ -8,3 +12,45 @@ var (
 	ErrIncorrectGeometry   = errors.New("wkbcommon: incorrect geometry")   // returned when unmarshalling WKB data into the wrong type (e. g. linestring into a point)
 	ErrUnsupportedGeometry = errors.New("wkbcommon: unsupported geometry") // returned when geometry type is not supported by this package
 )
+
+// ScanPoint takes binary wkb and decodes it into a point.
+func ScanPoint(data []byte) (geo.Point, int, error) {
+	order, typ, srid, geomData, err := unmarshalByteOrderType(data)
+	if err != nil {
+		return geo.Point{}, 0, err
+	}
+
+	switch typ {
+	case pointType:
+		if p, err := unmarshalPoint(order, geomData); err != nil {
+			return geo.Point{}, 0, err
+		} else {
+			return p, srid, nil
+		}
+	case multiPointType:
+		if mp, err := unmarshalMultiPoint(order, geomData); err != nil {
+			return geo.Point{}, 0, err
+		} else if len(mp) == 1 {
+			return mp[0], srid, nil
+		}
+	}
+
+	return geo.Point{}, 0, ErrIncorrectGeometry
+}
+
+// ScanMultiPoint takes binary wkb and decodes it into a multi-point.
+func ScanMultiPoint(data []byte) (geo.MultiPoint, int, error) {
+	m, srid, err := Unmarshal(data)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	switch p := m.(type) {
+	case geo.Point:
+		return geo.MultiPoint{p}, srid, nil
+	case geo.MultiPoint:
+		return p, srid, nil
+	}
+
+	return nil, 0, ErrIncorrectGeometry
+}
