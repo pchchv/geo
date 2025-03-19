6c8968fea6e1f0b7cfa1e0c661b35d7face75cb8
pchchv <ipchchv@gmail.com>
2025-03-19 10:03:12 +0300

    Add WKB encoder implementation for geometry encoding

--- encoding/wkb/wkb.go
+++ encoding/wkb/wkb.go
@@ -1,1 +1,23 @@
 package wkb
+
+import (
+	"encoding/binary"
+	"io"
+
+	"github.com/pchchv/geo/encoding/wkb/wkbcommon"
+)
+
+var DefaultByteOrder binary.ByteOrder = binary.LittleEndian // the order used for marshalling or encoding
+
+// An Encoder will encode a geometry as WKB to the writer given at
+// creation time.
+type Encoder struct {
+	e *wkbcommon.Encoder
+}
+
+// NewEncoder creates a new Encoder for the given writer.
+func NewEncoder(w io.Writer) *Encoder {
+	e := wkbcommon.NewEncoder(w)
+	e.SetByteOrder(DefaultByteOrder)
+	return &Encoder{e: e}
+}
