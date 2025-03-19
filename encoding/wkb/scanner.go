2d4201edfc52c8fe9856efc25b6ee44d4fdb8cf7
pchchv <ipchchv@gmail.com>
2025-03-19 10:37:21 +0300

    Add Scanner function for GeometryScanner to handle SQL query results

--- encoding/wkb/scanner.go
+++ encoding/wkb/scanner.go
@@ -30,6 +30,35 @@ type GeometryScanner struct {
 	Valid    bool // Valid is true if the geometry is not NULL
 }
 
+// Scanner return a GeometryScanner that can scan sql query results.
+// The geometryScanner.Geometry attribute will be set to the value.
+// If g is non-nil, it MUST be a pointer to an geo.Geometry type like a Point or LineString.
+// In that case the value will be written to g and the Geometry attribute.
+//
+//	var p geo.Point
+//	err := db.QueryRow("SELECT latlon FROM foo WHERE id=?", id).Scan(wkb.Scanner(&p))
+//	...
+//	// use p
+//
+// If the value may be null check Valid first:
+//
+//	var point geo.Point
+//	s := wkb.Scanner(&point)
+//	err := db.QueryRow("SELECT latlon FROM foo WHERE id=?", id).Scan(&s)
+//	...
+//	if s.Valid {
+//	  // use p
+//	} else {
+//	  // NULL value
+//	}
+//
+// Deprecated behavior: Scanning directly from MySQL columns is supported.
+// By default MySQL returns geometry data as WKB but prefixed with a 4 byte SRID.
+// To support this, if the data is not valid WKB, the code will strip the first 4 bytes and try again.
+func Scanner(g interface{}) *GeometryScanner {
+	return &GeometryScanner{g: g}
+}
+
 // Scan scanes the input []byte data into a geometry.
 // This could be into the geo geometry type pointer or, if nil,
 // the scanner.Geometry attribute.
