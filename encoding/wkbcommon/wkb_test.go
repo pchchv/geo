345fb45802168e82e5c4dcfdc0559224f4036fb1
pchchv <ipchchv@gmail.com>
2025-03-19 09:39:58 +0300

    Add comparison function for decoding and marshaling geometries in tests

--- encoding/wkbcommon/wkb_test.go
+++ encoding/wkbcommon/wkb_test.go
@@ -1,6 +1,7 @@
 package wkbcommon
 
 import (
+	"bytes"
 	"encoding/binary"
 	"testing"
 
@@ -18,5 +19,57 @@ func TestMarshal(t *testing.T) {
 func TestMustMarshal(t *testing.T) {
 	for _, g := range geo.AllGeometries {
 		MustMarshal(g, 0, binary.BigEndian)
+	}
+}
+
+func compare(t testing.TB, e geo.Geometry, s int, b []byte) {
+	t.Helper()
+	// Decoder
+	g, srid, err := NewDecoder(bytes.NewReader(b)).Decode()
+	if err != nil {
+		t.Fatalf("decoder: read error: %e", err)
+	}
+
+	if !geo.Equal(g, e) {
+		t.Errorf("decoder: incorrect geometry: %v != %v", g, e)
+	}
+
+	if srid != s {
+		t.Errorf("decoder: incorrect srid: %v != %v", srid, s)
+	}
+
+	// Umarshal
+	g, srid, err = Unmarshal(b)
+	if err != nil {
+		t.Fatalf("unmarshal: read error: %e", err)
+	}
+
+	if !geo.Equal(g, e) {
+		t.Errorf("unmarshal: incorrect geometry: %v != %v", g, e)
+	}
+
+	if srid != s {
+		t.Errorf("decoder: incorrect srid: %v != %v", srid, s)
+	}
+
+	var data []byte
+	if b[0] == 0 {
+		data, err = Marshal(g, s, binary.BigEndian)
+	} else {
+		data, err = Marshal(g, s, binary.LittleEndian)
+	}
+	if err != nil {
+		t.Fatalf("marshal error: %e", err)
+	}
+
+	if !bytes.Equal(data, b) {
+		t.Logf("%v", data)
+		t.Logf("%v", b)
+		t.Errorf("marshal: incorrent encoding")
+	}
+
+	// preallocation
+	if l := GeomLength(e, srid != 0); len(data) != l {
+		t.Errorf("prealloc length: %v != %v", len(data), l)
 	}
 }
