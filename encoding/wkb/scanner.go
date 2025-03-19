4088e1b68093541aebc08ab7b530e500c56d8ca0
pchchv <ipchchv@gmail.com>
2025-03-19 10:32:41 +0300

    Implement GeometryScanner to scan SQL query results into Geometry

--- encoding/wkb/scanner.go
+++ encoding/wkb/scanner.go
@@ -1,6 +1,13 @@
 package wkb
 
-import "github.com/pchchv/geo"
+import (
+	"database/sql"
+
+	"github.com/pchchv/geo"
+	"github.com/pchchv/geo/encoding/wkb/wkbcommon"
+)
+
+var _ sql.Scanner = &GeometryScanner{}
 
 // GeometryScanner scans the results of sql queries,
 // it can be used as a scan destination:
@@ -17,4 +24,35 @@ type GeometryScanner struct {
 	g        interface{}
 	Geometry geo.Geometry
 	Valid    bool // Valid is true if the geometry is not NULL
+}
+
+// Scan scanes the input []byte data into a geometry.
+// This could be into the geo geometry type pointer or, if nil,
+// the scanner.Geometry attribute.
+func (s *GeometryScanner) Scan(d interface{}) error {
+	if d == nil {
+		return nil
+	}
+
+	data, ok := d.([]byte)
+	if !ok {
+		return ErrUnsupportedDataType
+	}
+
+	s.Geometry = nil
+	s.Valid = false
+	if g, _, valid, err := wkbcommon.Scan(s.g, d); err == wkbcommon.ErrNotWKBHeader {
+		var e error
+		g, _, valid, e = wkbcommon.Scan(s.g, data[4:])
+		if e != wkbcommon.ErrNotWKBHeader {
+			err = e // nil or incorrect type, e.g. decoding line string
+		}
+	} else if err != nil {
+		return mapCommonError(err)
+	} else {
+		s.Geometry = g
+		s.Valid = valid
+	}
+
+	return nil
 }
