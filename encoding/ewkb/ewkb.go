b256150722c097a2cab17148f47073b821bad1ac
pchchv <ipchchv@gmail.com>
2025-03-20 10:02:55 +0300

    Add methods to set byte order and SRID, and implement geometry encoding in EWKB

--- encoding/ewkb/ewkb.go
+++ encoding/ewkb/ewkb.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"io"
 
+	"github.com/pchchv/geo"
 	"github.com/pchchv/geo/encoding/wkb/wkbcommon"
 )
 
@@ -29,4 +30,26 @@ func NewEncoder(w io.Writer) *Encoder {
 	e := wkbcommon.NewEncoder(w)
 	e.SetByteOrder(DefaultByteOrder)
 	return &Encoder{e: e, srid: DefaultSRID}
+}
+
+// SetByteOrder overrides the default byte order set when the encoder was created.
+func (e *Encoder) SetByteOrder(bo binary.ByteOrder) *Encoder {
+	e.e.SetByteOrder(bo)
+	return e
+}
+
+// SetSRID overrides the default srid.
+func (e *Encoder) SetSRID(srid int) *Encoder {
+	e.srid = srid
+	return e
+}
+
+// Encode writes the geometry encoded as EWKB to the given writer.
+func (e *Encoder) Encode(geom geo.Geometry, srid ...int) error {
+	s := e.srid
+	if len(srid) > 0 {
+		s = srid[0]
+	}
+
+	return e.e.Encode(geom, s)
 }
