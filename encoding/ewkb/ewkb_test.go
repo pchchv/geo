145ec65ae560840eb827f9c5cd5fbe1eb96ae39a
pchchv <ipchchv@gmail.com>
2025-03-20 10:04:57 +0300

    Add encoder benchmarks

---
+++ encoding/ewkb/ewkb_test.go
@@ -0,0 +1,37 @@
+package ewkb
+
+import (
+	"io"
+	"testing"
+
+	"github.com/pchchv/geo"
+)
+
+func BenchmarkEncode_Point(b *testing.B) {
+	g := geo.Point{1, 2}
+	e := NewEncoder(io.Discard)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := e.Encode(g)
+		if err != nil {
+			b.Fatalf("unexpected error: %v", err)
+		}
+	}
+}
+
+func BenchmarkEncode_LineString(b *testing.B) {
+	g := geo.LineString{
+		{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5},
+		{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5},
+	}
+	e := NewEncoder(io.Discard)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := e.Encode(g)
+		if err != nil {
+			b.Fatalf("unexpected error: %v", err)
+		}
+	}
+}
