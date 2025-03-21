ba8feb8499fade6b6aa0e604b64b46fc7076e790
pchchv <ipchchv@gmail.com>
2025-03-21 08:17:47 +0300

    Implement a writeLineString function

--- encoding/wkt/wkt.go
+++ encoding/wkt/wkt.go
@@ -1,1 +1,20 @@
 package wkt
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/pchchv/geo"
+)
+
+func writeLineString(buf *bytes.Buffer, ls geo.LineString) {
+	buf.WriteByte('(')
+	for i, p := range ls {
+		if i != 0 {
+			buf.WriteByte(',')
+		}
+
+		fmt.Fprintf(buf, "%g %g", p[0], p[1])
+	}
+	buf.WriteByte(')')
+}
